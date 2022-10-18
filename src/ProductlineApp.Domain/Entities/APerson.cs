00574f709ed5c4b9be944fdd1253ab9591b119e6
s21386 <s21386@pjwstk.edu.pl>
2022-10-18 18:45:28 +0200

    added initial entities

---
+++ src/ProductlineApp.Domain/Entities/APerson.cs
@@ -0,0 +1,11 @@
+﻿namespace ProductlineApp.Domain.Entities
+{
+    public abstract class APerson
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Nickname { get; set; }
+        public string Email { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ src/ProductlineApp.Domain/Enums/EProductCategory.cs
@@ -0,0 +1,22 @@
+﻿namespace ProductlineApp.Domain.Enums
+{
+    public enum EProductCategory
+    {
+        MOTOR,
+        ELECTRONICS,
+        COLLECTIBLES,
+        ART,
+        HOME,
+        GARDEN,
+        CLOTHING,
+        ACCESSORIES,
+        TOYS,
+        HOBBIES,
+        BOOKS,
+        MOVIES,
+        MUSIC,
+        HEALTH_AND_BEAUTY,
+        BUSINESS_AND_INDUSTRIAL,
+        PET_SUPPLIES
+    }
+}
