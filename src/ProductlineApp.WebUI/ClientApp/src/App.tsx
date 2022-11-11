8abd961330128f872d00f19d8d82d327f633ba75
s21347-Mateusz-Korba <s21347@pjwstk.edu.pl>
2022-11-11 17:25:51 +0100

    added sidebar component

--- src/ProductlineApp.WebUI/ClientApp/src/App.tsx
+++ src/ProductlineApp.WebUI/ClientApp/src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Sidebar from './components/common/sidebar/sidebar';
+import Dashboard from './pages/Dashboard';
+import Products from './pages/Products';
+import { Route, Routes } from 'react-router-dom'
+import Auctions from './pages/Auctions';
+import Orders from './pages/Orders';
+import Statistics from './pages/Statistics';
+import Settings from './pages/Settings';
+import Logout from './pages/Logout';
+import './App.css'
+import '../src/components/common/sidebar/sidebar.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacta
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar />
+      <div className="container" id='container'>
+        <Routes>
+          <Route path='/dashboard' element={<Dashboard />}></Route>
+          <Route path='/products' element={<Products />}></Route>
+          <Route path='/auctions' element={<Auctions />}></Route>
+          <Route path='/orders' element={<Orders />}></Route>
+          <Route path='/statistics' element={<Statistics />}></Route>
+          <Route path='/settings' element={<Settings />}></Route>
+          <Route path='/logout' element={<Logout />}></Route>
+        </Routes>
+      </div>
+    </>
   );
 }
 
