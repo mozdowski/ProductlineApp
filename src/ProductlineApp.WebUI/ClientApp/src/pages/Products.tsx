53d9e39abca5eb25564f6cf4a5dd634f01193888
s21386 <s21386@pjwstk.edu.pl>
2023-07-18 12:19:05 +0200

    fixed product call loop

--- src/ProductlineApp.WebUI/ClientApp/src/pages/Products.tsx
+++ src/ProductlineApp.WebUI/ClientApp/src/pages/Products.tsx
@@ -48,7 +48,7 @@ export default function Products(this: any) {
       }));
       setProducts(productsRecords);
     });
-  }, [searchValue, searchProducts]);
+  }, []);
 
   useLayoutEffect(() => {
     if (ref.current != null) {
